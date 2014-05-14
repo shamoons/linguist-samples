21039b4b29a2240371e816845d3e7ffa05748c4d
Shamoon Siddiqui <EmailShamoon@gmail.com>
2014-05-14 18:56:27 -0400

    Final path should work

--- index.js
+++ index.js
@@ -2,12 +2,7 @@ var fs = require('fs');
 var path = require('path');
 
 module.exports = function(filePath, cb) {
-  console.log(process.cwd());
-  console.log(__dirname);
-  console.log(path.join(__dirname, '../samples', filePath));
-  console.log(path.join('../samples', filePath));
-  console.log(path.join(__dirname, 'node_modules', 'linguist-samples', 'samples', filePath));
-  fs.readFile(path.join(__dirname, 'node_modules', 'linguist-samples', 'samples', filePath), function(err, fileData) {
+  fs.readFile(path.join(__dirname, 'samples', filePath), function(err, fileData) {
     cb(err, fileData);
   });
 }
