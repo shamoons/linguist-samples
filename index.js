19022029b380cba0235ddb4ea951aa260b8194be
Shamoon Siddiqui <EmailShamoon@gmail.com>
2014-05-14 18:38:58 -0400

    Created index to read the file

---
+++ index.js
@@ -0,0 +1,8 @@
+var fs = require('fs');
+var path = require('path');
+
+module.exports = function(filePath, cb) {
+  fs.readFile(path.join(__dirname, '../samples', filePath), function(err, fileData) {
+    cb(err, fileData);
+  });
+}
