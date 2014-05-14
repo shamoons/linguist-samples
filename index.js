122ba90347a7590570db9ab17cd47415137ed0be
Shamoon Siddiqui <EmailShamoon@gmail.com>
2014-05-14 18:54:30 -0400

    Fixing path

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ module.exports = function(filePath, cb) {
   console.log(__dirname);
   console.log(path.join(__dirname, '../samples', filePath));
   console.log(path.join('../samples', filePath));
-  console.log(path.join('./node_modules', 'linguist-samples', 'samples', filePath));
-  fs.readFile(path.join(__dirname, '../samples', filePath), function(err, fileData) {
+  console.log(path.join(__dirname, 'node_modules', 'linguist-samples', 'samples', filePath));
+  fs.readFile(path.join(__dirname, 'node_modules', 'linguist-samples', 'samples', filePath), function(err, fileData) {
     cb(err, fileData);
   });
 }
