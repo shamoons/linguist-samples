4c9574facde192e704a8718af78a22eced5fdb97
Shamoon Siddiqui <EmailShamoon@gmail.com>
2014-05-14 18:47:02 -0400

    Mode debubging

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ module.exports = function(filePath, cb) {
   console.log(process.cwd());
   console.log(__dirname);
   console.log(path.join(__dirname, '../samples', filePath));
+  console.log(path.join('../samples', filePath));
   fs.readFile(path.join(__dirname, '../samples', filePath), function(err, fileData) {
     cb(err, fileData);
   });
