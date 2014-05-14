fd511dd580d0d64981099c1478ae0ab5731c80a2
Shamoon Siddiqui <EmailShamoon@gmail.com>
2014-05-14 18:46:02 -0400

    Trying to modify the reading path

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var fs = require('fs');
 var path = require('path');
 
 module.exports = function(filePath, cb) {
+  console.log(process.cwd());
+  console.log(__dirname);
+  console.log(path.join(__dirname, '../samples', filePath));
   fs.readFile(path.join(__dirname, '../samples', filePath), function(err, fileData) {
     cb(err, fileData);
   });
