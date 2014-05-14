ad2947f3abbffe3562d91b79c0c4d1078791a18c
Shamoon Siddiqui <EmailShamoon@gmail.com>
2014-05-14 18:58:48 -0400

    Reading file UTF style

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var path = require('path');
 
 module.exports = function(filePath, cb) {
-  fs.readFile(path.join(__dirname, 'samples', filePath), function(err, fileData) {
+  fs.readFile(path.join(__dirname, 'samples', filePath), 'utf-8', function(err, fileData) {
     cb(err, fileData);
   });
 }
