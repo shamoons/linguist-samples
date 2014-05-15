356c42de85743520b0fb64ad5135180e876bf7c5
Shamoon Siddiqui <EmailShamoon@gmail.com>
2014-05-14 21:21:20 -0400

    var declaration

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var path = require('path');
 
 module.exports = function(filePath, cb) {
-  fileData = fs.readFileSync(path.join(__dirname, 'samples', filePath), 'utf-8');
+  var fileData = fs.readFileSync(path.join(__dirname, 'samples', filePath), 'utf-8');
   return fileData;
 
 }
