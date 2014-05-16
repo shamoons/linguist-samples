78df541c3b6dfe29cad755a4ac2f4d66ad4e27f2
Shamoon Siddiqui <EmailShamoon@gmail.com>
2014-05-15 22:01:59 -0400

    Created a class out of samples

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 var fs = require('fs');
 var path = require('path');
 
-module.exports = function(filePath, cb) {
-  var fileData = fs.readFileSync(path.join(__dirname, 'samples', filePath), 'utf-8');
-  return fileData;
+Samples = (function() {
+  Samples.prototype.readFile = function(filePath) {
+    var fileData = fs.readFileSync(path.join(__dirname, 'samples', filePath), 'utf-8');
+  }
 
-}
+  Samples.prototype.loadSampleFile = function(sampleFilePath) {
+    if(sampleFilePath === null) {
+      sampleFilePath = path.join(__dirname, 'samples.json');
+    }
+
+    var sampleFileData = fs.readFileSync(sampleFilePath, 'utf-8');
+
+    jsonData = JSON.parse(sampleFileData);
+  }
+})();
+
+module.exports = new Samples()
